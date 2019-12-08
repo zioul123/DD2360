e0a45ac0edc16fd85654e08016bf4dd5029e878a
zioul123 <zioul.rsmod123@gmail.com>
2019-12-08 22:50:19 +0100

    Convert mover_pc's struct pointer accesses to arguments

--- sputniPIC-master/src/Particles.cu
+++ sputniPIC-master/src/Particles.cu
@@ -76,10 +76,13 @@ void particle_deallocate(struct particles* part)
     by mover_PC() before calling it. */
 FPpart dt_sub_cycling, dto2, qomdt2;
 
-/** CPU serial function to move a single particle during one subcycle.
- *  i - particle index.
- */
-__host__ void h_move_particle(int i, struct particles* part, struct EMfield* field, struct grid* grd, struct parameters* param) 
+/** CPU serial function to move a single particle during one subcycle. */
+__host__ void h_move_particle(int i, int part_NiterMover, struct grid* grd, struct parameters* param,
+    FPpart* part_x, FPpart* part_y, FPpart* part_z, 
+    FPpart* part_u, FPpart* part_v, FPpart* part_w,
+    FPfield* field_Ex_flat, FPfield* field_Ey_flat, FPfield* field_Ez_flat,
+    FPfield* field_Bxn_flat, FPfield* field_Byn_flat, FPfield* field_Bzn_flat,
+    FPfield* grd_XN_flat, FPfield* grd_YN_flat, FPfield* grd_ZN_flat) 
 {
     // auxiliary variables
     FPpart omdtsq, denom, ut, vt, wt, udotb;
@@ -95,23 +98,23 @@ __host__ void h_move_particle(int i, struct particles* part, struct EMfield* fie
     // intermediate particle position and velocity
     FPpart xptilde, yptilde, zptilde, uptilde, vptilde, wptilde;
 
-    xptilde = part->x[i];
-    yptilde = part->y[i];
-    zptilde = part->z[i];
+    xptilde = part_x[i];
+    yptilde = part_y[i];
+    zptilde = part_z[i];
     // calculate the average velocity iteratively
-    for(int innter=0; innter < part->NiterMover; innter++){
+    for(int innter=0; innter < part_NiterMover; innter++){
         // interpolation G-->P
-        ix = 2 +  int((part->x[i] - grd->xStart)*grd->invdx);
-        iy = 2 +  int((part->y[i] - grd->yStart)*grd->invdy);
-        iz = 2 +  int((part->z[i] - grd->zStart)*grd->invdz);
+        ix = 2 +  int((part_x[i] - grd->xStart)*grd->invdx);
+        iy = 2 +  int((part_y[i] - grd->yStart)*grd->invdy);
+        iz = 2 +  int((part_z[i] - grd->zStart)*grd->invdz);
         
         // calculate weights
-        xi[0]   = part->x[i] - grd->XN_flat[get_idx(ix-1, iy, iz, grd->nyn, grd->nzn)];
-        eta[0]  = part->y[i] - grd->YN_flat[get_idx(ix, iy-1, iz, grd->nyn, grd->nzn)];
-        zeta[0] = part->z[i] - grd->ZN_flat[get_idx(ix, iy, iz-1, grd->nyn, grd->nzn)];
-        xi[1]   = grd->XN_flat[get_idx(ix, iy, iz, grd->nyn, grd->nzn)] - part->x[i];
-        eta[1]  = grd->YN_flat[get_idx(ix, iy, iz, grd->nyn, grd->nzn)] - part->y[i];
-        zeta[1] = grd->ZN_flat[get_idx(ix, iy, iz, grd->nyn, grd->nzn)] - part->z[i];
+        xi[0]   = part_x[i] - grd_XN_flat[get_idx(ix-1, iy, iz, grd->nyn, grd->nzn)];
+        eta[0]  = part_y[i] - grd_YN_flat[get_idx(ix, iy-1, iz, grd->nyn, grd->nzn)];
+        zeta[0] = part_z[i] - grd_ZN_flat[get_idx(ix, iy, iz-1, grd->nyn, grd->nzn)];
+        xi[1]   = grd_XN_flat[get_idx(ix, iy, iz, grd->nyn, grd->nzn)] - part_x[i];
+        eta[1]  = grd_YN_flat[get_idx(ix, iy, iz, grd->nyn, grd->nzn)] - part_y[i];
+        zeta[1] = grd_ZN_flat[get_idx(ix, iy, iz, grd->nyn, grd->nzn)] - part_z[i];
         for (int i = 0; i < 2; i++)
             for (int j = 0; j < 2; j++)
                 for (int k = 0; k < 2; k++)
@@ -123,40 +126,40 @@ __host__ void h_move_particle(int i, struct particles* part, struct EMfield* fie
         for (int ii=0; ii < 2; ii++)
             for (int jj=0; jj < 2; jj++)
                 for(int kk=0; kk < 2; kk++){
-                    Exl += weight[ii][jj][kk]*field->Ex_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
-                    Eyl += weight[ii][jj][kk]*field->Ey_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
-                    Ezl += weight[ii][jj][kk]*field->Ez_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
-                    Bxl += weight[ii][jj][kk]*field->Bxn_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
-                    Byl += weight[ii][jj][kk]*field->Byn_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
-                    Bzl += weight[ii][jj][kk]*field->Bzn_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
+                    Exl += weight[ii][jj][kk]*field_Ex_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
+                    Eyl += weight[ii][jj][kk]*field_Ey_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
+                    Ezl += weight[ii][jj][kk]*field_Ez_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
+                    Bxl += weight[ii][jj][kk]*field_Bxn_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
+                    Byl += weight[ii][jj][kk]*field_Byn_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
+                    Bzl += weight[ii][jj][kk]*field_Bzn_flat[get_idx(ix-ii, iy-jj, iz-kk, grd->nyn, grd->nzn)];
                 }
         
         // end interpolation
         omdtsq = qomdt2*qomdt2*(Bxl*Bxl+Byl*Byl+Bzl*Bzl);
         denom = 1.0/(1.0 + omdtsq);
         // solve the position equation
-        ut= part->u[i] + qomdt2*Exl;
-        vt= part->v[i] + qomdt2*Eyl;
-        wt= part->w[i] + qomdt2*Ezl;
+        ut= part_u[i] + qomdt2*Exl;
+        vt= part_v[i] + qomdt2*Eyl;
+        wt= part_w[i] + qomdt2*Ezl;
         udotb = ut*Bxl + vt*Byl + wt*Bzl;
         // solve the velocity equation
         uptilde = (ut+qomdt2*(vt*Bzl -wt*Byl + qomdt2*udotb*Bxl))*denom;
         vptilde = (vt+qomdt2*(wt*Bxl -ut*Bzl + qomdt2*udotb*Byl))*denom;
         wptilde = (wt+qomdt2*(ut*Byl -vt*Bxl + qomdt2*udotb*Bzl))*denom;
         // update position
-        part->x[i] = xptilde + uptilde*dto2;
-        part->y[i] = yptilde + vptilde*dto2;
-        part->z[i] = zptilde + wptilde*dto2;
+        part_x[i] = xptilde + uptilde*dto2;
+        part_y[i] = yptilde + vptilde*dto2;
+        part_z[i] = zptilde + wptilde*dto2;
         
         
     } // end of iteration
     // update the final position and velocity
-    part->u[i]= 2.0*uptilde - part->u[i];
-    part->v[i]= 2.0*vptilde - part->v[i];
-    part->w[i]= 2.0*wptilde - part->w[i];
-    part->x[i] = xptilde + uptilde*dt_sub_cycling;
-    part->y[i] = yptilde + vptilde*dt_sub_cycling;
-    part->z[i] = zptilde + wptilde*dt_sub_cycling;
+    part_u[i]= 2.0*uptilde - part_u[i];
+    part_v[i]= 2.0*vptilde - part_v[i];
+    part_w[i]= 2.0*wptilde - part_w[i];
+    part_x[i] = xptilde + uptilde*dt_sub_cycling;
+    part_y[i] = yptilde + vptilde*dt_sub_cycling;
+    part_z[i] = zptilde + wptilde*dt_sub_cycling;
 
 
     //////////
@@ -164,60 +167,60 @@ __host__ void h_move_particle(int i, struct particles* part, struct EMfield* fie
     ////////// BC
                                 
     // X-DIRECTION: BC particles
-    if (part->x[i] > grd->Lx){
+    if (part_x[i] > grd->Lx){
         if (param->PERIODICX==true){ // PERIODIC
-            part->x[i] = part->x[i] - grd->Lx;
+            part_x[i] = part_x[i] - grd->Lx;
         } else { // REFLECTING BC
-            part->u[i] = -part->u[i];
-            part->x[i] = 2*grd->Lx - part->x[i];
+            part_u[i] = -part_u[i];
+            part_x[i] = 2*grd->Lx - part_x[i];
         }
     }
                                                                 
-    if (part->x[i] < 0){
+    if (part_x[i] < 0){
         if (param->PERIODICX==true){ // PERIODIC
-           part->x[i] = part->x[i] + grd->Lx;
+           part_x[i] = part_x[i] + grd->Lx;
         } else { // REFLECTING BC
-            part->u[i] = -part->u[i];
-            part->x[i] = -part->x[i];
+            part_u[i] = -part_u[i];
+            part_x[i] = -part_x[i];
         }
     }
         
 
     // Y-DIRECTION: BC particles
-    if (part->y[i] > grd->Ly){
+    if (part_y[i] > grd->Ly){
         if (param->PERIODICY==true){ // PERIODIC
-            part->y[i] = part->y[i] - grd->Ly;
+            part_y[i] = part_y[i] - grd->Ly;
         } else { // REFLECTING BC
-            part->v[i] = -part->v[i];
-            part->y[i] = 2*grd->Ly - part->y[i];
+            part_v[i] = -part_v[i];
+            part_y[i] = 2*grd->Ly - part_y[i];
         }
     }
                                                                 
-    if (part->y[i] < 0){
+    if (part_y[i] < 0){
         if (param->PERIODICY==true){ // PERIODIC
-            part->y[i] = part->y[i] + grd->Ly;
+            part_y[i] = part_y[i] + grd->Ly;
         } else { // REFLECTING BC
-            part->v[i] = -part->v[i];
-            part->y[i] = -part->y[i];
+            part_v[i] = -part_v[i];
+            part_y[i] = -part_y[i];
         }
     }
                                                                 
     // Z-DIRECTION: BC particles
-    if (part->z[i] > grd->Lz){
+    if (part_z[i] > grd->Lz){
         if (param->PERIODICZ==true){ // PERIODIC
-            part->z[i] = part->z[i] - grd->Lz;
+            part_z[i] = part_z[i] - grd->Lz;
         } else { // REFLECTING BC
-            part->w[i] = -part->w[i];
-            part->z[i] = 2*grd->Lz - part->z[i];
+            part_w[i] = -part_w[i];
+            part_z[i] = 2*grd->Lz - part_z[i];
         }
     }
                                                                 
-    if (part->z[i] < 0){
+    if (part_z[i] < 0){
         if (param->PERIODICZ==true){ // PERIODIC
-            part->z[i] = part->z[i] + grd->Lz;
+            part_z[i] = part_z[i] + grd->Lz;
         } else { // REFLECTING BC
-            part->w[i] = -part->w[i];
-            part->z[i] = -part->z[i];
+            part_w[i] = -part_w[i];
+            part_z[i] = -part_z[i];
         }
     }
 }
@@ -233,10 +236,15 @@ int mover_PC(struct particles* part, struct EMfield* field, struct grid* grd, st
     dto2 = .5*dt_sub_cycling, qomdt2 = part->qom*dto2/param->c;
     
     // start subcycling
-    for (int i_sub=0; i_sub <  part->n_sub_cycles; i_sub++){
+    for (int i_sub=0; i_sub < part->n_sub_cycles; i_sub++){
         // move each particle with new fields
         for (int i=0; i < part->nop; i++){
-            h_move_particle(i, part, field, grd, param);
+            h_move_particle(i, part->NiterMover, grd, param,
+                part->x, part->y, part->z,
+                part->u, part->v, part->w, 
+                field->Ex_flat, field->Ey_flat, field->Ez_flat,
+                field->Bxn_flat, field->Byn_flat, field->Bzn_flat,
+                grd->XN_flat, grd->YN_flat, grd->ZN_flat);
         }  // end of one particle
     } // end of subcycling
                                                                         
