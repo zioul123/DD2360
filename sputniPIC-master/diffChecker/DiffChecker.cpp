20f976ff4a1e879f2f21d9e51f0c6d219f0d41d2
zioul123 <zioul.rsmod123@gmail.com>
2020-01-06 11:05:23 +0100

    Make diff checker take vtk files instead of diff

--- sputniPIC-master/diffChecker/DiffChecker.cpp
+++ sputniPIC-master/diffChecker/DiffChecker.cpp
@@ -7,53 +7,78 @@
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
+    if (argc < 3) {
         std::cout << "Need to provide the input file for diff checker." << std::endl;
-        std::cout << "Usage: ./DiffChecker.out abc.diff" << std::endl;
+        std::cout << "Usage: ./DiffChecker.out correct.vtk testing.vtk" << std::endl;
         return (-1);
     }
-    std::string inputFile = argv[1];
-    std::ifstream in(inputFile.c_str());
 
-    
-    if (in.is_open())
+    std::string inputFile1 = argv[1], inputFile2 = argv[2];
+    std::ifstream in1(inputFile1.c_str()); std::ifstream in2(inputFile2.c_str());
+  
+    std::string inLine;
+    double inDouble; 
+    std::queue<double> in1Doubles, in2Doubles;
+
+    // Read file 1
+    if (in1.is_open())
     {
-        std::string inLine;
-        double inDouble; 
-        std::queue<double> inLines;
-        std::queue<double> outLines;
-
-        while ( in.good() )
+        while ( in1.good() )
         {            
-            getline (in, inLine); 
-            if (in.eof()) break;
-            // std::cout << in << std::endl;
-            if (sscanf(inLine.c_str(), "< %lf", &inDouble) == 1) {
-                inLines.push(inDouble);
+            getline (in1, inLine); 
+            if (in1.eof()) break;
 
-            } else if (sscanf(inLine.c_str(), "> %lf", &inDouble) == 1) {
-                outLines.push(inDouble);
+            if (sscanf(inLine.c_str(), "%lf", &inDouble) == 1) {
+                in1Doubles.push(inDouble);
             } else {
                 continue;
             }
         }
-        in.close();
-        std::cout << "All input read." << std::endl;
-        while (inLines.size() > 0) 
-        {
-            // std::cout << inLines.front() << ", " << outLines.front() << std::endl;
-            if (fabs(outLines.front() - inLines.front()) > 10e-6) {
-                std::cout << "Big discrepancy in " << inputFile << " with number " << outLines.front() << " against " << inLines.front() << std::endl;
-                return 0;
-            }
-            inLines.pop(); outLines.pop();
-        }
-        std::cout << "All differences less than 10e-6." << std::endl;
+        in1.close();
+        std::cout << "All input1 read." << std::endl;
     } 
     else 
     {
-        std::cout << "Unable to open file." << std::endl;
+        std::cout << "Unable to open file 1." << std::endl;
+        return -1;
     }
+
+    // Read file 2
+    if (in2.is_open())
+    {
+        while ( in2.good() )
+        {            
+            getline (in2, inLine); 
+            if (in2.eof()) break;
+
+            if (sscanf(inLine.c_str(), "%lf", &inDouble) == 1) {
+                in2Doubles.push(inDouble);
+            } else {
+                continue;
+            }
+        }
+        in2.close();
+        std::cout << "All input2 read." << std::endl;
+    } 
+    else 
+    {
+        std::cout << "Unable to open file 2." << std::endl;
+        return -1;
+    }
+
+    // Compare the two
+    while (in1Doubles.size() > 0) 
+    {
+        // std::cout << in1Doubles.front() << ", " << in2Doubles.front() << std::endl;
+        
+        if (fabs(in1Doubles.front() - in2Doubles.front()) > 10e-6) {
+            std::cout << "Big discrepancy in " << inputFile1 << ", " << inputFile2 << " with number " << in1Doubles.front() << " against " << in2Doubles.front() << std::endl;
+            return 0;
+        }
+        in1Doubles.pop(); in2Doubles.pop();
+    }
+    std::cout << "All differences less than 10e-6." << std::endl;
+ 
     return 0;
 
 }
